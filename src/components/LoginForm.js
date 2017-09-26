e43b85f06ecfe5a649255ba46d91a99addb773b8
moshiko lev <moshiko.lev@gmail.com>
2017-09-27 00:09:57 +0300

    added elemets and  first design of login

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Button } from 'react-native-elements';
 import Card from './common/Card';
 import CardSection from './common/CardSection';
 
@@ -7,8 +8,20 @@ class LoginForm extends Component {
     render() {
         return (
             <Card>
-                <CardSection> 
+                <CardSection>
                     <Text> TESTTTTTTTTTTT</Text>
+                </CardSection>
+                <CardSection>
+                    <Text> TESTTTTTTTTTTT</Text>
+                </CardSection>
+                <CardSection>
+                    <Button
+                        raised
+                        icon={{ name: 'home', size: 32 }}
+                        buttonStyle={{ backgroundColor: 'red', borderRadius: 10 }}
+                        textStyle={{ textAlign: 'center' }}
+                        title={`Welcome to\nReact Native Elements`}
+                    />
                 </CardSection>
             </Card>
         );
